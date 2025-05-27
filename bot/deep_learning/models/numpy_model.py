fix false backprop in numpy model

bot/deep_learning/models/numpy_model.py
@@ -60,11 +60,11 @@ class Model:
     def __backpropagation(self, model_raw_hidden_output: np.ndarray, model_act_hidden_output: np.ndarray, model_raw_output: np.ndarray, input: np.ndarray, expected_output: np.ndarray) -> None:
         # calculate deltas
         loss                = model_raw_output - expected_output
-        delta_weight_2      = 1 / loss.size * loss.dot(model_act_hidden_output.T)
-        delta_bias_2        = (1 / loss.size) * np.sum(loss, axis=1, keepdims=True)
+        delta_weight_2      = loss.dot(model_act_hidden_output.T)
+        delta_bias_2        = np.sum(loss, axis=1, keepdims=True)           # in this case: batch_size = 1, db2 = loss, same to bd1 below
         delta_hidden        = self.main_weight_2.T.dot(loss) * self.__derivative_ReLU(model_raw_hidden_output)
-        delta_weight_1      = 1 / delta_hidden.size * delta_hidden.dot(input.T)
-        delta_bias_1        = (1 / delta_hidden.size) * np.sum(delta_hidden, axis=1, keepdims=True)
+        delta_weight_1      = delta_hidden.dot(input.T)
+        delta_bias_1        = np.sum(delta_hidden, axis=1, keepdims=True)
 
         # update weight and bias
         self.main_weight_1  = self.main_weight_1 - self.learning_rate * delta_weight_1